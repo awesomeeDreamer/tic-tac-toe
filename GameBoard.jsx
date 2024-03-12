22e4a67c2df98a7c2d1c8c61fdf56a38bc9fde7a
awesomeeDreamer <160412077+awesomeeDreamer@users.noreply.github.com>
2024-03-12 20:41:20 +0530

    Add files via upload

---
+++ GameBoard.jsx
@@ -0,0 +1,42 @@
+
+export default function GameBoard({ onSelectSquare,board }) {
+ 
+  //   const [gameBoard, setGameBoard] = useState(intialGameBoard);
+
+  //   function handleSeletSquare(rowIndex, colIndex) {
+  // setGameBoard((prevGameBoard) => {
+  //       const updatedBoard = [
+  //         ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //       ];
+  //       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //       return updatedBoard;
+  //     });
+
+  //     onSelectSquare();
+  //   }
+
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                {/* <button onClick={() => handleSeletSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button> */}
+
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
